3a4c35e98feaf15b63262751c997c5e24e2fe598
BertFrontEnd <Berton@yandex.ru>
2020-04-13 22:45:42 +0300

    feat: add decomposition

--- src/analytics.js
+++ src/analytics.js
@@ -4,7 +4,7 @@ function createAnalytics() {
 
   const listener = () => counter++;
 
-  document.addEventListener('click', listener)
+  document.addEventListener('click', listener);
   return {
     destroy() {
       document.removeEventListener('click', listener);
@@ -13,11 +13,11 @@ function createAnalytics() {
 
     getClicks() {
       if (isDestroyed) {
-        return "Analytics is destroyed";
+        return 'Analytics is destroyed. Total clicks = ${counter}';
       }
       return counter;
-    }
-  }
+    },
+  };
 }
 
 let analytics = createAnalytics();
